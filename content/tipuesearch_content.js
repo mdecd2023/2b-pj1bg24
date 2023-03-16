a3ba8785bd5b2dadbd3d67c7e277dd2010837ff1
41023213 <41023213@gm.nfu.edu.tw>
2023-03-16 14:53:09 +0800

    w

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'test \n OMG', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '41023218-陳岳檉 \n 41023213-張義聖 \n 個人倉儲： Here', 'tags': '', 'url': 'About.html'}]};
