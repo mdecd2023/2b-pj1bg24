d59486644c585ebf2debb39442c830e3933b7691
41023213 <41023213@gm.nfu.edu.tw>
2023-03-16 15:09:39 +0800

    ww

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '41023218-陳岳檉 \n 41023213-張義聖 \n 個人倉儲： Here', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '41023218-陳岳檉 \n 41023213-張義聖 \n 倉儲： https://github.com/mdecd2023/2b-pj1bg24 \n 網站 ： https://mdecd2023.github.io/2b-pj1bg24/content/index.html', 'tags': '', 'url': 'About.html'}]};
